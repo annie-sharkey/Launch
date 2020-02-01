c1a31a708491cbd4740f645fba6065ae5a5a8118
Jack Chandler <jtc4hnu@virginia.edu>
2020-02-01 13:38:00 -0500

    practice_completed

--- practice.js
+++ practice.js
@@ -3,3 +3,36 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+//Jack Chandler
+//1 February 2020
+//practice.js
+
+people_array = [
+    {
+        name : "Jack",
+        age : "18"
+    },
+
+    {
+        name : "Tom",
+        age : "20"
+    },
+
+    {
+        name : "Ladybug",
+        age : "11"
+    }
+]
+
+function GetNames (array) {
+    name_array = [];
+
+    array.forEach(element => {
+        name_array.push(element.name)
+    });
+
+    return name_array
+}
+console.log("Name Array:");
+console.log(GetNames(people_array));
