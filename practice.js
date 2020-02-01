9371a8149af3572cea8d7a676e6d9145a7d81b5b
Alan Li <al2hkm@virginia.edu>
2020-02-01 13:37:52 -0500

    add js file

--- practice.js
+++ practice.js
@@ -2,10 +2,22 @@
 Create an array of objects with properties name and age. 
 Make a function that accepts this array and returns an array of 
 just the names.
-
-var person = new Object();
-
-
-for(
-
 */
+
+var p1 = {name:"Alan", age:18};
+var p2 = {name:"Dakota", age:19};
+var p3 = {name:"Jim", age:54};
+
+var pepArray = {p1, p2, p3};
+
+function peopleNames(arr){
+    var pepNames =[];
+
+    for (let i=0; i<arr.length; i++){
+        pepNames.push(arr[i].name);
+    }
+
+    return pepNames;
+}
+
+console.log(peopleNames(pepArray));
