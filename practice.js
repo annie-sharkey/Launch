43b6d36fa368c0606c4a2aa34c6bd31753598c09
Emma Paris <kjk9cd@virginia.edu>
2020-02-01 13:35:30 -0500

    js stuff

--- practice.js
+++ practice.js
@@ -3,3 +3,19 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+var indiv = [{
+	name: "John",
+	age: 33
+}, {
+	name: "Tom",
+	age: 45
+}]
+
+function funct(indiv){
+    var names = [];
+    for(var i =0; i < indiv.length; i++){
+        names[i] = indiv[i].name;
+    }
+    return names;
+}
