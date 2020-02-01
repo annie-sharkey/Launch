aaedc6fc2a26d50dbfd9fa986d193f76ddeb0c18
Anthony Maringo <alm4cu@virginia.edu>
2020-02-01 13:15:56 -0500

    modified practice.js

--- practice.js
+++ practice.js
@@ -16,8 +16,6 @@ change
 change
 change
 change
-change
-change
 Create an array of objects with properties name and age. 
 Make a function that accepts this array and returns an array of 
 just the names.
