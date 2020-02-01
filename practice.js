44087b658b8629414c5bdf3dcb445b487150f551
Ann Hoang <lph2yr@virginia.edu>
2020-02-01 13:33:31 -0500

    Ann commit

--- practice.js
+++ practice.js
@@ -3,3 +3,33 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+var students = []
+let student1 = {
+        name : "Ann",
+        age : 20 };
+let student2 = {
+        name : "Justine",
+        age : 21 };
+let student3 = {
+        name : "Dan",
+        age : 25,
+}
+
+students.push( student1 )
+students.push( student2 )
+
+function searchName( ar ) {
+  const newLib = [];
+  for ( i in ar ){
+    var stu_name = ar[i].name;
+    newLib.push( stu_name );
+  }
+  return newLib;                                       //return the array newLib outside of the function when function is invoked
+}
+
+/* Testing
+var student_names = searchName(students)
+for ( stu in student_names ){
+  console.log( student_names[stu] );
+}
+*/
