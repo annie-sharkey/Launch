f2a86f673f31a905eb4acd8a41c8d9a991f2a173
Grace Barrett-Johnson <gb4yy@virginia.edu>
2020-02-01 13:32:00 -0500

    this is my branch with practice.js completed

--- practice.js
+++ practice.js
@@ -3,3 +3,24 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+let pairs = [
+    {name: "grace",
+     age: 18
+    },
+    {name: "sk",
+     age: 20
+    },
+    {name: "theo",
+     age:16
+    }
+];
+
+function example(allPairs){
+    let returnArr = [];
+    for(let i = 0; i < allPairs.length; i++){
+        returnArr.push(allPairs[i].name)
+    }
+    return returnArr;
+}
+print(example(pairs));
