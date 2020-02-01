6ada793633dc27a60755a4e6860b3aece9e97de9
Alex Shen <as5gd@viginia.edu>
2020-02-01 13:17:28 -0500

    did activity

--- practice.js
+++ practice.js
@@ -3,3 +3,20 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+let objects = [{
+    name: "bob",
+    age: 12
+},  {
+    name: "bob2",
+    age: 10
+}]
+
+function getNames(objects){
+    let output = []
+    objects.forEach((obj => {
+        output.push(obj["name"])
+    }))
+    return output
+}
+
+console.log(getNames(objects))
