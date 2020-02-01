08b481564fa32634d0d9cda09bce6b67d75fbb2b
Kyungjin Park <kjp8fj@virginia.edu>
2020-02-01 14:00:02 -0500

    practice completed

--- practice.js
+++ practice.js
@@ -3,3 +3,13 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+let ppl = [{name: "hehe", age: 10},{name: "hoho", age: 11}];
+
+let getNames = listOfPeople => {
+    for (let i = 0; i < listOfPeople.length; i++) {
+        console.log(listOfPeople[i].name);
+    }
+};
+
+getNames(ppl);
