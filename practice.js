9a9e24bde5b0f3c5f212f796b382739ad0add987
Tahsin <tk8dd@virginia.edu>
2020-02-01 13:32:29 -0500

    Tahsin Kazi branch

--- practice.js
+++ practice.js
@@ -3,3 +3,9 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+var temp = [["John",22],["Pablo",69],["Escobar",99]];
+
+function printer(array){
+    for (var i = 0; i < temp.length; i++) {
+        return temp[i][0];
+    }
