3b2bf5641104add4b11a49d7ff3ff4836a09c8c5
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-02-01 13:31:50 -0500

    Create function for just person names

--- practice.js
+++ practice.js
@@ -4,3 +4,26 @@ Make a function that accepts this array and returns an array of
 just the names.
 */
 
+let person1 = {
+    name: "Joe",
+    age: 15
+};
+
+let person2 = {
+    name: "Jim",
+    age: 29
+};
+
+let arr = {person1, person2};
+
+function justNames (arr) {
+    let returnedPeople = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        returnedPeople.push(arr[i].name);
+    }
+
+    return returnedPeople;
+};
+
+print(justNames(arr));
