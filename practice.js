fc789c0bab0a3797c9537024655236b90fa2b172
Kymane Llewellyn <enamyk@gmail.com>
2020-02-01 13:28:59 -0500

    practice.js

--- practice.js
+++ practice.js
@@ -3,3 +3,33 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+
+    var people = [
+        {
+        name: "Chris",
+        age: 18,
+        },
+        {
+        name: "Jamal",
+        age: 30,
+        },
+        {
+        name: "Trevor",
+        age: 21,
+        },
+    ];
+
+
+    function arrFunc(arrOfObj){
+        for (let key in people){
+            delete key.age;
+        }
+        return arrOfObj;
+    }
+
+
+    arrFunc(people);
+
+
+    
