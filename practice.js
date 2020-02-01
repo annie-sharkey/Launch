08869912c7c92245e15ac939cd5b583e46711fdc
Samuel Townsend <59664077+sft3hy@users.noreply.github.com>
2020-02-01 13:16:10 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -3,23 +3,4 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
-let personArr = [{
-	name: "Sam",
-	age: 20
-}, {
-	name: "Jack",
-	age: 25
-}, {
-	name: "Deborah",
-	age: 19
-}]
 
-function justNames(personArr){
-	let namesArr = [];
-	for(let i = 0; i < personArr.length; i++){
-		namesArr.push(personArr[i].name)
-	}
-	return namesArr;
-}
-
-console.log(justNames(personArr));
