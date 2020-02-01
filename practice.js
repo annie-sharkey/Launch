9f3a5fdb0b1814fbe35b686bc70cc11e68a23b89
acao <alc3pf@virginia.edu>
2020-02-01 13:18:37 -0500

    practice.js edited

--- practice.js
+++ practice.js
@@ -3,3 +3,17 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+let testArray = [p1 = {name:"Joe", age:34}, p2 = {name:"Suzy", age:38}, 
+p3 = {name:"Martin", age:20}, p4 = {name:"Caroline", age:23},
+p5 = {name:"Maria", age:25}]
+
+function getNames(peopleArray) {
+    var answerArray = []
+    for (i = 0; i < peopleArray.length; i++) {
+        answerArray.push(peopleArray[i].name)
+    }
+    return answerArray
+}
+
+console.log(getNames(testArray))
