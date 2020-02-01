4c043885158d8c20fb87f8baa5126de75f177b09
Dakota Bourne <db2nb@virginia.edu>
2020-02-01 13:14:18 -0500

    made function and array

--- practice.js
+++ practice.js
@@ -3,3 +3,19 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+let objects = {
+person1:{name: "Jason Bourne", age: 22}, 
+person2:{name: "Melissa Jameson", age:18}, 
+person3:{name:"John Adams", age:285}
+}
+
+
+function nameList(list){
+  var objnames = []
+    for(let i in list){
+        objnames.push(list[i].name);
+    }
+  return objnames
+}
+
+console.log(nameList(objects));
