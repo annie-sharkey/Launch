856c42cb9a3558b8a4993d44a6e0b8220501db8f
Dan Rayder <dsr8cm@virginia.edu>
2020-02-01 13:25:16 -0500

    practice

--- practice.js
+++ practice.js
@@ -3,3 +3,24 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+
+var p1 = {name:"Jim", age:"20"};
+var p2 = {name:"Jan", age:"40"};
+var p3 = {name:"Jeff", age:"60"};
+
+
+let our_array = []
+
+
+function nameSearch(our_array){ 
+  let retArray = []; // Empty Array to add names to
+
+
+  for (let i = 0; i < our_array.length; i++){
+       retArray.push(our_array[i][0]);
+   }
+  // When the loop is finished, return retArray
+  return retArray;
+}
+
