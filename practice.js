c4b94b35bc556f00b0c2eeb82ee347217b0edea0
Kellan Delaney <ked2pf@virginia.edu>
2020-02-01 13:15:55 -0500

    javascript activity

--- practice.js
+++ practice.js
@@ -3,3 +3,19 @@ Create an array of objects with properties name and age.
 Make a function that accepts this array and returns an array of 
 just the names.
 */
+
+let people = [
+    {name: "Kellan", age: 18},
+    {name: "Frank", age: 22},
+    {name: "Alice", age: 58},
+    {name: "Tina", age: 33},
+    {name: "Jimmy", age: 15} 
+];
+
+names = (persons) => {
+    let namesArr = [];
+    for (let i=0; i<persons.length; i++) {
+        namesArr[i]=person[i].name;
+    }
+    return namesArr;
+}
