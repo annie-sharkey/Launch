7210fa83e8550a8278d1f300ee3e022f754e3fee
Nile Alkebulan <nba5ne@virginia.edu>
2020-02-01 13:40:43 -0500

    practice code

--- practice.js
+++ practice.js
@@ -2,4 +2,21 @@
 Create an array of objects with properties name and age. 
 Make a function that accepts this array and returns an array of 
 just the names.
+
 */
+var peoples =[{
+    name: "John",
+    age: 19
+},
+    {name: "jake",
+    age: 20,
+}
+]
+function printNames(peoples){
+    nameArray=[]
+    for(people of peoples){
+        var n = people['name']
+        nameArray.push(n)
+    }
+    return nameArray
+}
