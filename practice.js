be32b5c6a035e2f18add53a226a603d20785db5b
Matt Thompson <mattluthompson@gmail.com>
2020-02-01 13:37:02 -0500

    message goes here

--- practice.js
+++ practice.js
@@ -1,6 +1,36 @@
 /*
-Create an array of objects with properties name and age. 
-Make a function that accepts this array and returns an array of 
+Create an array of objects with properties name and age.
+Make a function that accepts this array and returns an array of
 just the names.
 */
+let person1 = {
+  name: "Matt",
+  age: 19
+};
 
+let person2 = {
+  name: "Alex",
+  age: 20
+};
+
+let person3 = {
+  name: "Nick",
+  age: 21
+};
+
+let person4 = {
+  name: "John",
+  age: 22
+};
+
+let newArray = [person1, person2, person3, person4];
+
+function nameOutput(arr){
+  let outputArray = [];
+  for (let index = 0; index<arr.length; index++){
+    outputArray.push(arr[index]["name"]);
+  };
+  return outputArray;
+}
+
+console.log(nameOutput(newArray))
